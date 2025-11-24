c2f2ef45c22864ab2f594abc97e29ad766173176
David-Wijaya-241402040 <180739063+David-Wijaya-241402040@users.noreply.github.com>
2025-11-24 18:09:26 +0700

    Main Structure

---
+++ src/main/java/com/project/app/App.java
@@ -0,0 +1,3 @@
+package main.java.com.project.app;
+
+public class App { public static void main(String[] args) { Main.launch(Main.class, args); } }
---
+++ src/main/java/com/project/app/config/DBConnection.java
@@ -0,0 +1,4 @@
+package main.java.com.project.app.config;
+
+public class DBConnection {
+}
